7be44ace35b3f32b490c28d1f85c91652002dbca
tanghuibo <1215893264@QQ.COM>
2019-01-30 00:32:01 +0800

    增加ui界面

---
+++ spring-takeaway/spring-takeaway-boot-ui/src/main/java/io/github/tanghuibo/springtakeawaybootui/config/AutoUiConfig.java
@@ -0,0 +1,20 @@
+package io.github.tanghuibo.springtakeawaybootui.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * @description: 配置信息类
+ * @author: tanghuibo aa18984850147@qq.com
+ * @create: 2019-01-30 00:08
+ **/
+
+@Configuration
+public class AutoUiConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/**").addResourceLocations("classpath:/META-INF/spring-takeaway-ui/");
+    }
+}
